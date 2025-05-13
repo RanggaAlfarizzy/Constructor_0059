0ac34366a28eb17a4baa953f0ae00ed9dadc3b5f
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-13 17:14:17 +0800

    Menambahkan constructor kepada Class mahasiswa

--- constructor.cpp
+++ constructor.cpp
@@ -6,3 +6,13 @@ public:
 mahasiswa(); //Constructor
 };
 
+mahasiswa::mahasiswa(){
+    cout<<"Constructor Terpanggil"<<endl; // definisi Constructor
+    };
+    
+    int main(){
+        mahasiswa mhs; //Pembuatan Object
+    
+        return 0;
+    }
+
