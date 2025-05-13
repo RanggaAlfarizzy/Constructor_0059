c470cbcc7a6cecfd2f2bd0eead761f1f861f3f25
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-13 17:33:26 +0800

    untuk mencoba metode chain diatas

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -12,3 +12,10 @@ class buku{
         return this->judul;
     }
 };
+
+int main ()
+{
+    buku bukunya;
+    cout<<bukunya.setJudul("Matematika").getJudul(); //chain function calls
+    return 0;
+}
