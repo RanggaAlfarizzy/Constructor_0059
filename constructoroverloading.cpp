1d00e199dae49f594cf471734b9e115d625218e8
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-13 17:22:50 +0800

    Menambahkan constructor dengan definisi NIM dan Nama

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -23,3 +23,8 @@ mahasiswa ::mahasiswa(int iNim){
 mahasiswa ::mahasiswa(string iNama){
     nama = iNama; // definisi hanya Nama
 }
+
+mahasiswa ::mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi NIM dan Nama
+    nama = iNama;
+}
