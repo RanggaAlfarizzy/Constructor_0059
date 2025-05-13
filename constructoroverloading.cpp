9938305c5b5a99d21f7baf52ad557d5313823653
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-13 17:25:39 +0800

    method cetak

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -34,3 +34,16 @@ void mahasiswa::cetak(){
     cout << endl << "Nim = " <<nim << endl;
     cout << "Nama = " << nama << endl;
 }
+
+int main() {
+    mahasiswa mhs1(102030);  // Hanya NIM
+    mahasiswa mhs2("Andi"); // Hanya nama
+    mahasiswa mhs3(010203, "Santi"); // NIM dan Nama
+
+    //Tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
